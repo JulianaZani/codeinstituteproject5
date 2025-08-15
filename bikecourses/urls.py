feb28fd89a56dbd2eca88eb7fbad834ef3e2697a
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-08-15 13:23:22 +0100

    Add courses path in urls.py, register courses in settings.py, and update All Courses button link

--- bikecourses/urls.py
+++ bikecourses/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),
+    path('courses/', include('courses.urls')),
 ]
