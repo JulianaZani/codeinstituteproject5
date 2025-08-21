bd48166d5961fb7d020bf8c65ef7dd7ec7e0f5a9
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-08-21 11:18:26 +0100

    Fix 404 error, update urls.py, MEDIA in settings.py and thumbnails images names

--- bikecourses/settings.py
+++ bikecourses/settings.py
@@ -130,12 +130,12 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files (User-uploaded content)
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 
--- courses/models.py
+++ courses/models.py
@@ -22,9 +22,7 @@ class Course(models.Model):
     category = models.CharField(max_length=50, choices=CATEGORIES)
     level = models.CharField(max_length=20, choices=LEVELS)
     duration_hours = models.PositiveIntegerField(default=0)
-    thumbnail = models.ImageField(
-        upload_to='courses/thumbnails/', blank=True, null=True
-    )
+    thumbnail = models.ImageField(upload_to="courses/thumbnails/", blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
