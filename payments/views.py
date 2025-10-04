cb2853aa92a98196d6f0171a3a21847c5b1a577d
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-10-04 08:42:56 +0100

    Create PaymentIntent in payments/views.py

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from checkout.models import Order
+from .services import create_payment_intent_for_order
 
-# Create your views here.
+
+def payment_view(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    pi = create_payment_intent_for_order(order)
+
+    context = {
+        "order": order,
+        "client_secret": pi.client_secret,
+        "stripe_publishable_key": settings.STRIPE_PUBLISHABLE_KEY,
+    }
+    return render(request, "payments/payment.html", context)
